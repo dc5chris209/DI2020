ed7bc6e66065facec44c62d0967d63d1918cda1e
dc5chris209 <61719205+dc5chris209@users.noreply.github.com>
2020-03-03 18:30:33 -0700

    Add files via upload

---
+++ 02_hello_world.py
@@ -0,0 +1,8 @@
+print "Hello Expert Services Kickoff 2020 - Reaching Out to The World"
+
+print "Gathering The Latest Virus News"
+
+f=open("data.txt", "r")
+if f.mode == 'r':
+    contents = f.read ()
+    print (contents)
